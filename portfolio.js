7306988d050679067b1cde9c3e4f1c544742389d
ValeriaCorradi <142531057+ValeriaCorradi@users.noreply.github.com>
2024-09-01 22:39:42 +0100

    block3

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+// scroll to section
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: "smooth" });
+  //_________________CREATE SCROLL ANIMATION___________________________________
+}
