eae20bc9b824fb06e035a8ce280720dab90b9cc1
ValeriaCorradi <142531057+ValeriaCorradi@users.noreply.github.com>
2024-09-02 17:00:05 +0100

    create separate page for footer

--- portfolio.js
+++ portfolio.js
@@ -4,3 +4,15 @@ function scrollToSection(sectionId) {
   section.scrollIntoView({ behavior: "smooth" });
   //_________________CREATE SCROLL ANIMATION___________________________________
 }
+
+// Function to load the footer
+function loadFooter() {
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector('#footer').innerHTML = data;
+    });
+}
+
+// Call the function to load the footer
+loadFooter();
